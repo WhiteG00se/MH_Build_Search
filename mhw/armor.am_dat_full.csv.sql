8b1c4789ab4d74698a950a55f9856ba32205560f
White Goose <t.lauppe@outlook.com>
2024-05-26 14:28:29 +0200

    init

---
+++ mhw/armor.am_dat_full.csv.sql
@@ -0,0 +1,23 @@
+SELECT
+  a.name,
+  a.variant,
+  a.equid_slot,
+  a.defense,
+  a.fire_res,
+  a.water_res,
+  a.ice_res,
+  a.thunder_res,
+  a.dragon_res,
+  a.slot_count,
+  a.slot_1_size,
+  a.slot_2_size,
+  a.slot_3_size,
+  a.set_skill_1,
+  a.skill_1,
+  a.skill_1_level,
+  a.skill_2,
+  a.skill_2_level,
+  a.skill_3,
+  a.skill_3_level
+WHERE
+  (a.defense > 0 || a.type.toLowerCase() == 'charm') & & a.name != 'Unavailable'
