c3ca53707d033c1b7b316120d8d45633d7a59f8c
White Goose <t.lauppe@outlook.com>
2024-06-03 22:27:22 +0200

    add skill data table

---
+++ mhw/skill_data.skl_dat.csv.sql
@@ -0,0 +1,11 @@
+SELECT
+  a.id.replace(/ ^ \ d +: /, '') AS id,
+  a.level,
+  a.unlock_skill_1,
+  a.unlock_skill_2,
+  a.unlock_skill_3,
+  a.unlock_skill_4,
+  a.unlock_skill_5,
+  a.unlock_skill_6
+WHERE
+  ! a.id.includes('Unavailable') && ! a.id.includes('--------')
